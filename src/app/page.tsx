7b62715c0b3f852adaee1016b01e849b9b395660
Fandika <fandika.ikhsan@gmail.com>
2024-08-13 12:45:12 +0700

    chore: add bg gray in home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import DashboardContainer from "@/containers/DashboardContainer"
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-between p-24">
+    <div className="flex min-h-screen bg-gray-900 flex-col items-center justify-between p-10">
       <DashboardContainer />
     </div>
   )
